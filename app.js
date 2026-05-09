ccf5c18f3029058725729cc9e4dcebbe5a110003
Hanamomon <145081281+Hanamomon@users.noreply.github.com>
2026-05-08 17:56:30 -0700

    set ejs for views

--- app.js
+++ app.js
@@ -7,4 +7,7 @@ app.use(express.static(assetsPath));
 
 app.use(express.urlencoded({ extended: true }));
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.listen(3000);
